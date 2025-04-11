1ca97fbcb58cafabc6e7dd3ec10d79c28ced0732
MbabaziOwen <163897840+MbabaziOwen@users.noreply.github.com>
2025-04-11 19:06:33 +0300

    done
    done

---
+++ python essentials 1/SpathiphyllumLAB3.1.10.py
@@ -0,0 +1,22 @@
+"""
+Spathiphyllum, more commonly known as a peace lily or white sail plant, is one of the most popular indoor houseplants that filters out harmful toxins from the air. Some of the toxins that it neutralizes include benzene, formaldehyde, and ammonia.
+
+Imagine that your computer program loves these plants. Whenever it receives an input in the form of the word Spathiphyllum, it involuntarily shouts to the console the following string: "Spathiphyllum is the best plant ever!"
+
+Write a program that utilizes the concept of conditional execution, takes a string as input, and:
+
+    prints the sentence "Yes - Spathiphyllum is the best 
+    plant ever!" to the screen if the inputted string is "Spathiphyllum" (upper-case)
+    prints "No, I want a big Spathiphyllum!" if the inputted string is "spathiphyllum" (lower-case)
+    prints "Spathiphyllum! Not [input]!" otherwise. Note: [input] is the string taken as input.
+"""
+#start
+input_user = input("start typing...")
+
+if input_user == "Spathiphyllum":
+    print("Yes - Spathiphyllum is the best plant ever!")
+
+elif input_user == "spathiphyllum":
+    print("No, I want a big Spathiphyllum!" )
+else:
+    print("Spathiphyllum! Not [input]!")
